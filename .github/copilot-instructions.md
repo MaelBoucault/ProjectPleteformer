0306d2fdebceb471a001f6a4943e51f66a183bfa
MaelBoucault <maelboucault@gmail.com>
2025-05-26 08:14:05 +0200

    26-05-2025__8:14

--- .github/copilot-instructions.md
+++ .github/copilot-instructions.md
@@ -2,7 +2,7 @@
 - Project name: ProjectPleteformer
 - Unity version: Unity 6000.0.32f1
 - Active game object:
-  - Name: Barde
-  - Tag: Untagged
+  - Name: Player
+  - Tag: Player
   - Layer: Default
 <!-- UNITY CODE ASSIST INSTRUCTIONS END -->
--- Assets/Main/Scripte/CollisionPlayer.cs
+++ Assets/Main/Scripte/CollisionPlayer.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionPlayer : MonoBehaviour
 {
@@ -7,6 +8,12 @@ public class CollisionPlayer : MonoBehaviour
         if (collision.gameObject.tag == "Fall")
         {
             gameObject.GetComponent<PlayerHealth>().UpdateHealth(-gameObject.GetComponent<PlayerHealth>().MaxHealth, new Vector3(0,0,0));
+        }
+
+        if (collision.gameObject.tag == "Respawn")
+        {
+            gameObject.transform.position = new Vector3(0,0,0);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
 
     }
